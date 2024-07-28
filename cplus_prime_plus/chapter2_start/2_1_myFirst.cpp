f2554b58928397842ed4f0433a8a79314f4c6148
“664163046@qq.com” <664163046sd>
2024-07-28 22:15:17 +0800

    update

---
+++ cplus_prime_plus/chapter2_start/2_1_myFirst.cpp
@@ -0,0 +1,16 @@
+//
+//  2_1_myFirst.cpp
+//  
+//
+//  Created by 孙迪 on 2024/7/28.
+//
+
+#include <iostream>  // a PREPROCESSOR directive
+int main()  // function header
+{  // start of function body
+    using namespace std;  // make definitions visible
+    cout << "Come up and C++ me some time";  //message
+    cout << endl;  // make a new line
+    cout << "You won't regret it!" << endl;  //more output
+    return 0;  // terminate main()
+}  // end of function body
