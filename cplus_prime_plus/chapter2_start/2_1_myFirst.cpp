755af2b8fb40db259cc360008988230792f24f8f
“664163046@qq.com” <664163046sd>
2024-07-29 23:28:23 +0800

    update

--- cplus_prime_plus/chapter2_start/2_1_myFirst.cpp
+++ cplus_prime_plus/chapter2_start/2_1_myFirst.cpp
@@ -5,12 +5,12 @@
 //  Created by 孙迪 on 2024/7/28.
 //
 
-#include <iostream>  // a PREPROCESSOR directive
-int main()  // function header
-{  // start of function body
-    using namespace std;  // make definitions visible
-    cout << "Come up and C++ me some time";  //message
-    cout << endl;  // make a new line
-    cout << "You won't regret it!" << endl;  //more output
-    return 0;  // terminate main()
-}  // end of function body
+// #include <iostream>  // a PREPROCESSOR directive
+// int main()  // function header
+// {  // start of function body
+//     using namespace std;  // make definitions visible
+//     cout << "Come up and C++ me some time";  //message
+//     cout << endl;  // make a new line
+//     cout << "You won't regret it!" << endl;  //more output
+//     return 0;  // terminate main()
+// }  // end of function body
